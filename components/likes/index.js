1e06806e18beb4e775db45cd16ef4266ea435a1a
panpanxiong3 <15160190801@163.com>
2020-07-06 16:20:44 +0800

    20200706 component-like

--- components/likes/index.js
+++ components/likes/index.js
@@ -4,20 +4,35 @@ Component({
    * 组件的属性列表
    */
   properties: {
-
+    count: {
+      type: Number,
+      value: 8
+    },
+    isLike: {
+      type: Boolean,
+    }
   },
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    yesImg: "images/like.png",
+    noImg: "images/like@dis.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike: function (event) {
+      let count = this.properties.count;
+      let isLike = this.properties.isLike;
+      count = isLike ? count + 1 : count - 1;
+      this.setData({
+        count: count,
+        isLike: !isLike
+      })
+    }
   }
 })
