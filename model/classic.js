b4fd035cd8016098b687f43425203be9a5d3b04f
unknown <unknown@example.com>
2021-01-13 21:57:23 +0800

    左右切换获取或设置缓存信息

--- model/classic.js
+++ model/classic.js
@@ -3,6 +3,10 @@ import {
 } from "../util/http";
 
 class Classic extends HTTP {
+  /**
+   * 获取最新一期期刊信息
+   * @param {*} sCoadBack 回调参数
+   */
   getLates(sCoadBack) {
     this.request({
       url: 'classic/latest',
@@ -12,14 +16,30 @@ class Classic extends HTTP {
       }
     });
   }
-
-  getClassic(index,isPrevious, sCoadBack) {
-    this.request({
-      url: "classic/" + index + (isPrevious.toString()=="previous"?"/previous":"/next"),
-      success: (res) => {
-        sCoadBack(res);
-      }
-    })
+/**
+ * 获取api信息
+ * @param {*} index 当前期刊
+ * @param {*} isPrevious 点击方向 “previous” or “next”
+ * @param {*} sCoadBack 回调参数
+ */
+  getClassic(index, isPrevious, sCoadBack) {
+    //获取点击左右方向键后的期刊数，对应“+1”or“-1”
+    let classicKey = isPrevious == "next" ? this._getkey(index + 1) : this._getkey(index - 1);
+    //获取缓存
+    let classics = wx.getStorageSync(classicKey);
+    //无缓存，调用api并设置缓存
+    if (!classics) {
+      this.request({
+        url: "classic/" + index + "/" + isPrevious,
+        success: (res) => {
+          sCoadBack(res);
+          wx.setStorageSync(classicKey, res);
+        }
+      })     
+    } else {
+      //存在缓存信息即回调
+       sCoadBack(classics)
+    }
   }
   /**
    * 判断是否为最新一期
@@ -49,7 +69,17 @@ class Classic extends HTTP {
   _getLasterIndex() {
     return wx.getStorageSync('laster')
   }
+  /**
+   * 回调缓存信息名称 
+   * @param {*} index 当前期刊数
+   */
+  _getkey(index) {
+    let key = 'classic_' + index;
+    return key
+  }
 }
+
+
 
 export {
   Classic
