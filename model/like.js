22faa6e78bfb453e39964b5c2b8674c2955f1dab
panpanxiong3 <15160190801@163.com>
2021-03-23 15:18:42 +0800

    提交代码

--- model/like.js
+++ model/like.js
@@ -1,18 +1,18 @@
 import {
   HTTP
-} from '../util/http-p'
+} from '../util/http'
 
 class LikeModel extends HTTP {
-  like(behavior, artID, category){
-      let url = behavior == 'like' ? 'like' : 'like/cancel'
-      this.request({
-          url: url,
-          method: 'POST',
-          data: {
-              art_id: artID,
-              type: category
-          }
-      })
+  like(behavior, artID, category) {
+    let url = behavior == 'like' ? 'like' : 'like/cancel'
+    this.request({
+      url: url,
+      method: 'POST',
+      data: {
+        art_id: artID,
+        type: category
+      }
+    })
   }
   /**
    * like组件调用获取数据
@@ -20,11 +20,11 @@ class LikeModel extends HTTP {
    * @param {*} category type方式
    * @param {*} sCallBack 回调参数
    */
-  getClassicLikeStatus(artId,category,sCallBack){
+  getClassicLikeStatus(artId, category, sCallBack) {
     let url = `classic/${category}/${artId}/favor`;
     this.request({
-      url:url,
-      success:sCallBack
+      url: url,
+      success: sCallBack
     })
   }
 
