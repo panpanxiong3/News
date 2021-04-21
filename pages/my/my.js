f222caa9e28e477b6b5bf0db7eb0129db43a2829
panpanxiong3 <15160190801@163.com>
2021-04-21 22:16:34 +0800

    提交代码

--- pages/my/my.js
+++ pages/my/my.js
@@ -5,26 +5,15 @@ Page({
    * 页面的初始数据
    */
   data: {
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-// 查看是否授权
-wx.getSetting({
-  success (res){
-    if (res.authSetting['scope.userInfo']) {
-      // 已经授权，可以直接调用 getUserInfo 获取头像昵称
-      wx.getUserInfo({
-        success: function(res) {
-          console.log(res.userInfo)
-        }
-      })
-    }
-  }
-})
+
+    this.userAuthorized();
   },
 
   /**
@@ -79,7 +68,35 @@ wx.getSetting({
    * 获取用户授权信息
    * @param {} event 授权回调信息
    */
-  bindGetUserInfo:function(e){
-     console.log('回调信息',e.detail.userInfo);
+  bindGetUserInfo(e) {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认
+    // 开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    // wx.getUserProfile({
+    //   desc: '用于完善会员资料', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+    //   success: (res) => {
+    //     this.setData({
+    //       userInfo: res.userInfo,
+    //       hasUserInfo: true
+    //     })
+    //   }
+    // })
+  },
+
+  userAuthorized() {
+    wx.getSetting({
+      withSubscriptions: true,
+      success: data => {
+        if (data.authSetting['scope.userInfo']) {
+          // 查看是否授权
+          wx.getUserInfo({
+            success: data => {
+              console.log("用户信息", data)
+            }
+          })
+        }else{
+          console.log('err');
+        }
+      }
+    })
   }
 })
